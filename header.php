6e8fcde65ace390596b43b77db5459a83d302631
Peepskeleton <96471753+Peepskeleton@users.noreply.github.com>
2023-02-17 11:18:31 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP Project 01</title>
+        <link rel="stylesheet" href="reset.css">
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+<div>
+    <form action="includes/login.inc.php" method="post">
+        <input type="text" name="mailuid" placeholder="Username/E-mail...">
+        <input type="password" name="pwd" placeholder="Password...">
+        <button type="submit" name="login-submit">Login</button>
+<form action="includes/logout.inc.php" method="post">
+    <button type="submit" name="logout-submit">logout</button> 
+</div>
+</header>
+
+
+<section class="index-categories">
+    <div class="index-categories-list">
+</section>
+</body>
+</html> 
